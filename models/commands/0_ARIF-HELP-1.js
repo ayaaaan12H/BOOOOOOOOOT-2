b8fd52d2c9878a129c8b63ec62b0f8309f75bcc0
ayaaaan12H <ayaankhaaan32@gmail.com>
2026-04-28 18:34:06 +0500

    Update bot creator name in help command message

--- models/commands/0_ARIF-HELP-1.js
+++ models/commands/0_ARIF-HELP-1.js
@@ -96,7 +96,7 @@ module.exports.run = async function ({ api, event, args, getText }) {
 PAGE [${page}/${Math.ceil(arrayInfo.length / numberOfOnePage)}]
 COMMAND DETAIL: ${prefix}help <command>
 
-🤖 THIS BOT IS MADE BY MR ARIF BABU 🙂
+🤖 THIS BOT IS MADE BY 𝐂𝐑𝐔𝐒𝐇ゝ🩷 🙂
 `;
 
   return api.sendMessage(msg, threadID, messageID);
